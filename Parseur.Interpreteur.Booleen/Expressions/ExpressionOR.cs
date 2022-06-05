4e96d7ff21009e4405eac4d2318821a6b70f62fb
ConcreteImplementation <2095110@csfoy.ca>
2022-06-05 12:10:28 -0400

    TDD ok pour expression OR

---
+++ Parseur.Interpreteur.Booleen/Expressions/ExpressionOR.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parseur.Interpreteur.Booleen.Expressions
+{
+    internal class ExpressionOR : ExpressionBinaire<bool>
+    {
+        public ExpressionOR(int debut, int fin) : base(debut, fin)
+        {
+        }
+
+        public override int Priorite => 100;
+        protected override bool resoudre()
+            => gauche.Resoudre() || droite.Resoudre();
+    }
+}
